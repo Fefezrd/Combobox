536c7917f24b6df0f6b576b26f00a1cbca382802
Federico Zardetto <103184360+Fefezrd@users.noreply.github.com>
2022-12-15 12:03:50 +0100

    Risoluzione
    Risolto esercizio JS e aggiunta Calcolatrice nella seconda pagina

--- ComboCascata/js/regioni.js
+++ ComboCascata/js/regioni.js
@@ -1,5 +1,5 @@
 "use strict";
-//Prova mia
+//Prova mia ->
 // const sb = document.querySelector('#regione')
 // btn.onchange = (event) => {
 //     event.preventDefault();
@@ -8,14 +8,14 @@
 const regioni = ["1-Umbria","2-Lazio","3-Toscana","4-Marche","5-Abruzzo"];
 const province = ["1-Perugia", "1-Terni","2-Roma","2-Viterbo", "2-Rieti", "2-Latina", "2-Frosinone","3-Fiuggi"];
 const daticomune = [
-    {"idRegione":"1", "nomeProvincia":"Perugia", "comuni":["Gubbio", "Folignano"]},
-    {"idRegione":"1", "nomeProvincia":"Terni", "comuni":["Narni", "Folignano"]},
-    {"idRegione":"2", "nomeProvincia":"Roma", "comuni":["Grottaferrata", "Folignano"]},
-    {"idRegione":"2", "nomeProvincia":"Viterbo", "comuni":["Vetralla", "Folignano"]},
-    {"idRegione":"3", "nomeProvincia":"Firenze", "comuni":["Gubbio", "Folignano"]},
-    {"idRegione":"3", "nomeProvincia":"Livorno", "comuni":["Gubbio", "Folignano"]},
-    {"idRegione":"4", "nomeProvincia":"Ancona", "comuni":["Gubbio", "Folignano"]},
-    {"idRegione":"4", "nomeProvincia":"Ascoli Piceno", "comuni":["Gubbio", "Folignano"]},
+    {"idRegione":1, "nomeProvincia":"Perugia", "comuni":["Gubbio", "Folignano"]},
+    {"idRegione":1, "nomeProvincia":"Terni", "comuni":["Narni", "Folignano"]},
+    {"idRegione":2, "nomeProvincia":"Roma", "comuni":["Grottaferrata", "Folignano"]},
+    {"idRegione":2, "nomeProvincia":"Viterbo", "comuni":["Vetralla", "Folignano"]},
+    {"idRegione":3, "nomeProvincia":"Firenze", "comuni":["Gubbio", "Folignano"]},
+    {"idRegione":3, "nomeProvincia":"Livorno", "comuni":["Gubbio", "Folignano"]},
+    {"idRegione":4, "nomeProvincia":"Ancona", "comuni":["Gubbio", "Folignano"]},
+    {"idRegione":4, "nomeProvincia":"Ascoli Piceno", "comuni":["Gubbio", "Folignano"]},
 ];
 
 
@@ -24,10 +24,6 @@ function inizializzazione() {
     var comboRegione = document.getElementById("regione");
     var comboProvincia = document.getElementById("provincia");
     var comboComune = document.getElementById("comune");
-
-    comboRegione.style.display = "block";
-    comboProvincia.style.display = "none";
-    comboComune.style.display = "none";
 
     var datidtringajson = JSON.stringify(daticomune);
     var datiobjfromjsonstring = JSON.parse(datidtringajson);
@@ -45,9 +41,9 @@ function getProvincia(idRegione) {
     // const provincia = province.find(x => x.value.startsWith(idRegione));
     const provincia = [];
     for(let item of province) { 
-          if(item.startsWith(idRegione)) {
+        if(item.startsWith(idRegione)) {
                 provincia.push(item);
-          }
+        }
     } 
     // console.log(provincia[0]);
     var selProvincia = document.getElementById("provincia");
@@ -55,13 +51,12 @@ function getProvincia(idRegione) {
     removeOptions(selProvincia);
     
     for(var i = 0; i < provincia.length; i++) {
-          var opt = provincia[i];
-          var el = document.createElement("option");
-          el.textContent = opt;
-          el.value = opt;
-          selProvincia.appendChild(el);
+        var opt = provincia[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        selProvincia.appendChild(el);
     }
-    selProvincia.style.display = "block";
 }
 
 function getComune(provincia) {
@@ -84,34 +79,33 @@ function getComune(provincia) {
     removeOptions(selComune);
     
     for(var i = 0; i < comune.length; i++) {
-          var opt = comune[i];
-          var el = document.createElement("option");
-          el.textContent = opt;
-          el.value = opt;
-          selComune.appendChild(el);
+        var opt = comune[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        selComune.appendChild(el);
     }
-    selComune.style.display = "block";
 }
 
 function removeOptions(selectElement) {
-    debugger;
     var i, L = selectElement.options.length - 1;
     for(i = L; i >= 0; i--) {
-          if(i > 0) {
-                selectElement.remove(i);
-          }
+        if(i > 0) {
+            selectElement.remove(i);
+        }
     }
 }
 
 
 //Serve a far vedere nell'HTML il risultato della Combobox 
-function visualizzaScelte(){
+function VisualizzaScelte(){
     var selRegione = document.getElementById("regione");
     var selProvincia = document.getElementById("provincia");
     var selComune = document.getElementById("comune");
-    let scelte = 'Hai scelto: ' + selRegione.options[selRegione.selectedIndex].indexText + ' - ' + selProvincia.value + ' - ' +   selComune.value;
+    let scelte = 'Hai scelto: ' + selRegione.options[selRegione.selectedIndex].value + ' - ' + selProvincia.value + ' - ' +   selComune.value;
     console.log(scelte);
     var divp = document.getElementById("result");
     // x scrivere nella pagina html
     divp.innerText = scelte;
 }
+
